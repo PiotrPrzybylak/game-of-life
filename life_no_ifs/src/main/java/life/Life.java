f0b5a283b462e1f3509b112c5d7dd33da701de9a
Piotr Przybylak <piotr.przybylak@gmail.com>
2014-02-20 12:52:06 +0000

    Copied test from life2

---
+++ life_no_ifs/src/main/java/life/Life.java
@@ -0,0 +1,17 @@
+package life;
+
+public class Life {
+
+
+    public void setInitialBoard(char[][] initalWorld) {
+
+    }
+
+    public char[][] getCurrentBoard() {
+        return null;
+    }
+
+    public void evolve() {
+
+    }
+}
